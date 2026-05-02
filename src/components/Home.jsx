a987fcefa0b8ef4e96ea739befe0f1d06bab1d7b
SanthoshKumarS29 <santhoshvk090@gmail.com>
2026-05-02 12:04:31 +0530

    Add a updated projects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,6 @@ const Home = () => {
     return (
         <div id='hero'>
             <div className='h-screen place-content-center py-10 lg:py-16 px-lg-12 banner'>
-                {/* <div className='grid md:grid-cols-2 items-center justify-center'>
-                    <div className='flex flex-col gap-4 justify-center text-white'>
-                        <p className='text-[#7a50eb] font-bold'>Hi, I am</p>
-                        <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>SANTHOSH KUMAR</h1>
-                        <p className='text-xl'>Building digital experiences with careful coding and creative design. Welcome to my world, where every detail matters</p>
-                        <button className='border-2 border-[#7a50eb] w-[200px] rounded-md font-bold my-6 md:mx-0 py-3 text-[#7a50eb] hover:bg-[#7a50eb] hover:text-black ease-in-out duration-500'><a href="#Contact">Hire Me</a></button>
-                    </div>
-                    <img src={hero} alt="Hero Image" className="lg:w-[300px] mx-auto py-6 w-[300px] rounded-[50%] hidden md:block" />
-                </div> */}
                 <div className='text-center space-y-7 max-w-[1240px] mx-auto '>
                     <h1 className='text-4xl md:text-7xl lg:text-9xl uppercase hover:tracking-wider duration-200 ease-in-out'><span className='text-[#7a50eb]'>S</span><span className='text-xl md:text-5xl lg:text-8xl'>anthosh</span> <br className='hidden lg:block'/><span className='text-[#7a50eb]'>K</span><span className='text-xl md:text-5xl lg:text-8xl'>umar</span></h1>
                     <p className='md:text-end pr-5'><a href="#Project">— <span className='hover:text-[#7a50eb] cursor-pointer duration-300 ease-in-out'>Frontend Developer</span> | <span className='hover:text-[#7a50eb] cursor-pointer duration-300 ease-in-out'>React & Tailwind Enthusiast</span></a></p>
